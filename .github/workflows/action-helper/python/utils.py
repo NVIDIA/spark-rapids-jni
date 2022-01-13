9e60a0c2992b7c54af9ee29fe8249aca2e7717c5
Peixin Li <pxli@nyu.edu>
2022-01-13 13:13:55 +0800

    Init CICD setup
    Signed-off-by: Peixin Li <pxli@nyu.edu>

---
+++ .github/workflows/action-helper/python/utils.py
@@ -0,0 +1,152 @@
+# Copyright (c) 2022, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+import os
+import sys
+
+import requests
+
+# constants
+API_URL = 'https://api.github.com'
+
+
+class PullRequest:
+    """Pull Request class"""
+
+    def __init__(self,
+                 head_owner, head, head_token,
+                 base_owner, repo, base, base_token):
+        self.head_owner = head_owner
+        self.head = head
+        self.base_owner = base_owner
+        self.base_repo = repo
+        self.base = base
+        self.pulls_url = f'{API_URL}/repos/{self.base_owner}/{self.base_repo}/pulls'
+        self._head_auth_headers = {
+            'Accept': 'application/vnd.github.v3+json',
+            'Authorization': f"token {head_token}"
+        }
+        self._base_auth_headers = {
+            'Accept': 'application/vnd.github.v3+json',
+            'Authorization': f"token {base_token}"
+        }
+
+    def get_open(self):
+        """get open pull request if existed"""
+        params = {
+            'state': 'open',
+            'head': f"{self.head_owner}:{self.head}",
+            'base': self.base,
+        }
+        print(params)
+        r = requests.get(self.pulls_url, headers=self._base_auth_headers, params=params)
+        if r.status_code == 200:
+            return r.json()
+        if r.status_code == 304:
+            return None
+        # FAILURE
+        print('FAILURE - list PR')
+        print(f'status code: {r.status_code}')
+        raise Exception(f"Failed to create PR: {r.json()}")
+
+    def create(self, params):
+        """create a pull request"""
+        # the token here must have write access to head owner/repo
+        r = requests.post(self.pulls_url, headers=self._head_auth_headers, json=params)
+        if r.status_code == 201:
+            print('SUCCESS - create PR')
+            pull = r.json()
+            number = str(pull['number'])
+            sha = str(pull['head']['sha'])
+            return number, sha, False
+        if r.status_code == 422:  # early-terminate if no commits between HEAD and BASE
+            print('SUCCESS - No commits')
+            print(r.json())
+            return '', '', True
+        # FAILURE
+        print('FAILURE - create PR')
+        print(f'status code: {r.status_code}')
+        raise Exception(f"Failed to create PR: {r.json()}")
+
+    def merge(self, number, params):
+        """merge a pull request"""
+        # the token here must have write access to base owner/repo
+        url = f'{self.pulls_url}/{number}/merge'
+        return requests.put(url, headers=self._head_auth_headers, json=params)
+
+    def auto_merge(self, number, sha):
+        """merge a auto-merge pull request"""
+        params = {
+            'sha': sha,
+            'merge_method': 'merge'
+        }
+        r = self.merge(number, params)
+        if r.status_code == 200:
+            self.comment(number, '**SUCCESS** - auto-merge')
+            print('SUCCESS - auto-merge')
+            return
+        else:
+            print('FAILURE - auto-merge')
+            self.comment(number=number, content=f"""**FAILURE** - Unable to auto-merge. Manual operation is required.
+```
+{r.json()}
+```
+
+Please use the following steps to fix the merge conflicts manually:
+```
+# Assume upstream is {self.base_owner}/{self.base_repo} remote
+git fetch upstream {self.head} {self.base}
+git checkout -b fix-auto-merge-conflict-{number} upstream/{self.base}
+git merge upstream/{self.head}
+# Fix any merge conflicts caused by this merge
+git commit -am "Merge {self.head} into {self.base}"
+git push <personal fork> fix-auto-merge-conflict-{number}
+# Open a PR targets {self.base_owner}/{self.base_repo} {self.base}
+```
+**IMPORTANT:** Before merging this PR, be sure to change the merging strategy to `Create a merge commit` (repo admin only).
+
+Once this PR is merged, the auto-merge PR should automatically be closed since it contains the same commit hashes
+""")
+            print(f'status code: {r.status_code}')
+            raise Exception(f"Failed to auto-merge PR: {r.json()}")
+
+    def comment(self, number, content):
+        """comment in a pull request"""
+        url = f'{API_URL}/repos/{self.base_owner}/{self.base_repo}/issues/{number}/comments'
+        params = {
+            'body': content
+        }
+        r = requests.post(url, headers=self._base_auth_headers, json=params)
+        if r.status_code == 201:
+            print('SUCCESS - create comment')
+        else:
+            print('FAILURE - create comment')
+            print(f'status code: {r.status_code}')
+            print(r.json())
+
+
+class EnvDefault(argparse.Action):
+    """EnvDefault argparse action class"""
+
+    def __init__(self, env, default=None, required=True, **kwargs):
+        if not default and env:
+            if env in os.environ:
+                default = os.environ[env]
+        if required and default:
+            required = False
+        super(EnvDefault, self).__init__(default=default, required=required, **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        setattr(namespace, self.dest, values)
---
+++ ci/deploy.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+#
+# Copyright (c) 2022, NVIDIA CORPORATION. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+###
+# Script to deploy cudf jar files along with other classifiers,
+# such as cudaXXX, sources, javadoc.
+#
+# Argument(s):
+#   SIGN_FILE: true/false, whether to sign the jar/pom file to de deployed
+#
+# Used environment(s):
+#   CLASSIFIERS:    The classifier list of the jars to be deployed
+#   SERVER_ID:      The repository id for this deployment.
+#   SERVER_URL:     The url where to deploy artifacts.
+#   GPG_PASSPHRASE: The passphrase used to sign files, only required when <SIGN_FILE> is true.
+###
+
+set -ex
+
+SIGN_FILE=$1
+OUT_PATH=$WORKSPACE/$OUT
+
+cd $WORKSPACE/
+REL_VERSION=$(mvn exec:exec -q --non-recursive -Dexec.executable=echo -Dexec.args='${project.version}')
+
+echo "REL_VERSION: $REL_VERSION, OUT_PATH: $OUT_PATH \
+SERVER_URL: $SERVER_URL, SERVER_ID: $SERVER_ID"
+
+###### Build the deploy command ######
+if [ "$SIGN_FILE" == true ]; then
+    DEPLOY_CMD="mvn -B gpg:sign-and-deploy-file -s ci/settings.xml -Dgpg.passphrase=$GPG_PASSPHRASE"
+else
+    DEPLOY_CMD="mvn -B deploy:deploy-file -s ci/settings.xml"
+fi
+echo "Deploy CMD: $DEPLOY_CMD"
+
+###### Build types/files from classifiers ######
+FPATH="$OUT_PATH/rapids-4-spark-jni-$REL_VERSION"
+CLASS_TYPES=''
+CLASS_FILES=''
+ORI_IFS="$IFS"
+IFS=','
+for CLASS in $CLASSIFIERS; do
+    CLASS_TYPES="${CLASS_TYPES},jar"
+    CLASS_FILES="${CLASS_FILES},${FPATH}-${CLASS}.jar"
+done
+# Remove the first char ','
+CLASS_TYPES=${CLASS_TYPES#*,}
+CLASS_FILES=${CLASS_FILES#*,}
+IFS="$ORI_IFS"
+
+###### Copy jar so we strip off classifier  #######
+# Use the first classifier(aka jar file) as the default jar
+FIRST_FILE=${CLASS_FILES%%,*}
+cp -f "$FIRST_FILE" "$FPATH.jar"
+
+###### Deploy cudf jar with all its additions ######
+$DEPLOY_CMD -Durl=$SERVER_URL -DrepositoryId=$SERVER_ID \
+            -Dfile=$FPATH.jar -DpomFile=spark-rapids-jni/pom.xml \
+            -Dfiles=$CLASS_FILES \
+            -Dtypes=$CLASS_TYPES \
+            -Dclassifiers=$CLASSIFIERS
