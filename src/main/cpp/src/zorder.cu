3d095799c1a2c01817d62f88950d6dd1812e4c75
Jason Lowe <jlowe@nvidia.com>
2022-11-23 11:02:58 -0600

    Fix build after make_offsets_child_column moved in rapidsai/cudf#12185
    Signed-off-by: Jason Lowe <jlowe@nvidia.com>

--- src/main/cpp/src/zorder.cu
+++ src/main/cpp/src/zorder.cu
@@ -17,7 +17,7 @@
 #include "zorder.hpp"
 
 #include <cudf/column/column_factories.hpp>
-#include <cudf/strings/detail/utilities.cuh>
+#include <cudf/strings/detail/strings_children.cuh>
 #include <cudf/table/table_device_view.cuh>
 #include <cudf/types.hpp>
 
