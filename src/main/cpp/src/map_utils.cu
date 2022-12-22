db64a99d6b43f50669354485b9cfac4aba79675d
Nghia Truong <nghiatruong.vn@gmail.com>
2022-12-21 21:31:20 -0800

    Add empty files

---
+++ src/main/cpp/src/map_utils.cu
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2022, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <rmm/cuda_stream_view.hpp>
+#include <rmm/exec_policy.hpp>
+
+#include "map_utils.hpp"
+
+namespace {
+
+// todo
+
+} // namespace
+
+namespace spark_rapids_jni {
+
+std::unique_ptr<cudf::column> from_json(cudf::column_view const &input,
+                                        bool throw_if_keys_duplicate, rmm::cuda_stream_view stream,
+                                        rmm::mr::device_memory_resource *mr) {
+
+  return nullptr;
+}
+
+} // namespace spark_rapids_jni
