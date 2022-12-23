7349cb74c4e5afa40014bd8ee95c7adc1752ba61
Nghia Truong <nghiatruong.vn@gmail.com>
2022-12-22 19:18:19 -0800

    Misc

--- src/main/cpp/src/map_utils.cu
+++ src/main/cpp/src/map_utils.cu
@@ -18,6 +18,7 @@
 
 //
 #include <limits>
+#include <sstream>
 
 #include <string_view>
 
@@ -32,9 +33,19 @@
 #include <cudf/strings/strings_column_view.hpp>
 #include <cudf_test/column_utilities.hpp>
 #include <rmm/cuda_stream_view.hpp>
+
+//
 #include <rmm/exec_policy.hpp>
 
-#define PRINT_DEBUG_JSON
+//
+#include <thrust/copy.h>
+#include <thrust/count.h>
+#include <thrust/iterator/transform_iterator.h>
+#include <thrust/scan.h>
+
+#define DEBUG_FROM_JSON
+
+namespace spark_rapids_jni {
 
 namespace {
 
@@ -87,9 +98,31 @@ auto unify_json_strings(cudf::column_view const &input, rmm::cuda_stream_view st
   return output;
 }
 
-} // namespace
+template <typename InputIterator, typename StencilIterator, typename OutputIterator,
+          typename Predicate>
+OutputIterator thrust_copy_if(rmm::exec_policy policy, InputIterator first, InputIterator last,
+                              StencilIterator stencil, OutputIterator result, Predicate pred) {
+  auto const copy_size = std::min(static_cast<std::size_t>(std::distance(first, last)),
+                                  static_cast<std::size_t>(std::numeric_limits<int>::max()));
 
-namespace spark_rapids_jni {
+  auto itr = first;
+  while (itr != last) {
+    auto const copy_end =
+        static_cast<std::size_t>(std::distance(itr, last)) <= copy_size ? last : itr + copy_size;
+    result = thrust::copy_if(policy, itr, copy_end, stencil, result, pred);
+    stencil += std::distance(itr, copy_end);
+    itr = copy_end;
+  }
+  return result;
+}
+
+template <typename InputIterator, typename OutputIterator, typename Predicate>
+OutputIterator thrust_copy_if(rmm::exec_policy policy, InputIterator first, InputIterator last,
+                              OutputIterator result, Predicate pred) {
+  return thrust_copy_if(policy, first, last, first, result, pred);
+}
+
+} // namespace
 
 std::unique_ptr<cudf::column> from_json(cudf::column_view const &input,
                                         bool throw_if_keys_duplicate, rmm::cuda_stream_view stream,
@@ -100,22 +133,147 @@ std::unique_ptr<cudf::column> from_json(cudf::column_view const &input,
 
   // Firstly, concatenate all the input json strings into one giant input json string.
   // The output can be validated using https://jsonformatter.curiousconcept.com/.
-  auto const processed_input = unify_json_strings(input, stream, default_mr);
+  auto const unified_json_buff = unify_json_strings(input, stream, default_mr);
+  auto const d_unified_json =
+      cudf::device_span<char const>{unified_json_buff.data(), unified_json_buff.size()};
 
-#ifdef PRINT_DEBUG_JSON
-  auto h_json = cudf::detail::make_host_vector_sync(
-      cudf::device_span<char const>{processed_input.data(), processed_input.size()}, stream);
-  std::cout << "Processed json string:\n";
-  std::cout << std::string_view{h_json.data(), h_json.size()} << std::endl;
+#ifdef DEBUG_FROM_JSON
+  {
+    auto const h_json = cudf::detail::make_host_vector_sync(d_unified_json, stream);
+    std::stringstream ss;
+    ss << "Processed json string:\n";
+    ss << std::string_view{h_json.data(), h_json.size()};
+    std::cerr << ss.str() << std::endl;
+  }
 #endif
 
-#if 0
   // Tokenize the input json strings.
-  auto const d_input =
-      cudf::device_span<SymbolT const>{processed_input.data(), processed_input.size()};
+  using namespace cudf::io::json;
+  static_assert(sizeof(SymbolT) == sizeof(char),
+                "Invalid internal data for nested json tokenizer.");
   auto const [tokens, token_indices] =
-      cudf::io::json::detail::get_token_stream(d_input, default_options, stream);
+      detail::get_token_stream(d_unified_json, cudf::io::json_reader_options{}, stream, default_mr);
+
+#ifdef DEBUG_FROM_JSON
+  {
+    auto const h_tokens = cudf::detail::make_host_vector_sync(
+        cudf::device_span<PdaTokenT const>{tokens.data(), tokens.size()}, stream);
+    auto const h_token_indices = cudf::detail::make_host_vector_sync(
+        cudf::device_span<SymbolOffsetT const>{token_indices.data(), token_indices.size()}, stream);
+
+    std::stringstream ss;
+    ss << "Tokens:\n";
+    for (auto const token : h_tokens) {
+      ss << static_cast<int>(token) << ", ";
+    }
+    ss << "\nToken indices:\n";
+    for (auto const token_idx : h_token_indices) {
+      ss << static_cast<int>(token_idx) << ", ";
+    }
+    std::cerr << ss.str() << std::endl;
+  }
+#endif
+
+  // Check for error.
+  CUDF_EXPECTS(thrust::count(rmm::exec_policy(stream), tokens.begin(), tokens.end(),
+                             token_t::ErrorBegin) == 0,
+               "There is error during parsing the input json string(s).");
+
+  // Whether a token does represent a node in the tree representation
+  auto const is_node = [] __device__(PdaTokenT const token) -> bool {
+    switch (token) {
+      case token_t::StructBegin:
+      case token_t::ListBegin:
+      case token_t::StringBegin:
+      case token_t::ValueBegin:
+      case token_t::FieldNameBegin:
+      case token_t::ErrorBegin: return true;
+      default: return false;
+    };
+  };
+
+  // Whether the token pops from the parent node stack
+  auto const does_pop = [] __device__(PdaTokenT const token) -> bool {
+    switch (token) {
+      case token_t::StructMemberEnd:
+      case token_t::StructEnd:
+      case token_t::ListEnd: return true;
+      default: return false;
+    };
+  };
+
+  // Whether the token pushes onto the parent node stack
+  auto const does_push = [] __device__(PdaTokenT const token) -> bool {
+    switch (token) {
+      case token_t::FieldNameBegin:
+      case token_t::StructBegin:
+      case token_t::ListBegin: return true;
+      default: return false;
+    };
+  };
+
+  auto const num_tokens = tokens.size();
+  auto const num_nodes =
+      thrust::count_if(rmm::exec_policy(stream), tokens.begin(), tokens.end(), is_node);
+
+  // Node levels: transform_exclusive_scan, copy_if.
+  rmm::device_uvector<TreeDepthT> node_levels(num_nodes, stream, default_mr);
+  {
+    rmm::device_uvector<TreeDepthT> token_levels(num_tokens, stream, default_mr);
+    auto const push_pop_it = thrust::make_transform_iterator(
+        tokens.begin(), [does_push, does_pop] __device__(PdaTokenT const token) -> cudf::size_type {
+          return does_push(token) - does_pop(token);
+        });
+    thrust::exclusive_scan(rmm::exec_policy(stream), push_pop_it, push_pop_it + num_tokens,
+                           token_levels.begin());
+#ifdef DEBUG_FROM_JSON
+    {
+      auto const h_json = cudf::detail::make_host_vector_sync(d_unified_json, stream);
+      auto const h_token_indices = cudf::detail::make_host_vector_sync(
+          cudf::device_span<SymbolOffsetT const>{token_indices.data(), token_indices.size()},
+          stream);
+      auto const h_token_levels = cudf::detail::make_host_vector_sync(
+          cudf::device_span<TreeDepthT const>{token_levels.data(), token_levels.size()}, stream);
+
+      std::stringstream ss;
+      ss << "Token levels:\n";
+      SymbolOffsetT print_idx{0};
+      for (size_t i = 0; i < h_token_levels.size(); ++i) {
+        auto const token_idx = h_token_indices[i];
+        while (print_idx < token_idx) {
+          ss << print_idx << ": " << h_json[print_idx] << " | -- \n";
+          ++print_idx;
+        }
+        auto const c = h_json[token_idx];
+        auto const level = h_token_levels[i];
+        ss << token_idx << ": " << c << " | " << static_cast<int>(level) << " (node)\n";
+      }
+      std::cerr << ss.str() << std::endl;
+    }
+#endif
+
+    auto const node_levels_end =
+        thrust_copy_if(rmm::exec_policy(stream), token_levels.begin(), token_levels.end(),
+                       tokens.begin(), node_levels.begin(), is_node);
+    CUDF_EXPECTS(thrust::distance(node_levels.begin(), node_levels_end) == num_nodes,
+                 "Node level count mismatch");
+  }
+
+#ifdef DEBUG_FROM_JSON
+  {
+    auto const h_node_levels = cudf::detail::make_host_vector_sync(
+        cudf::device_span<TreeDepthT const>{node_levels.data(), node_levels.size()}, stream);
+    std::stringstream ss;
+    ss << "Node levels:\n";
+    for (auto const level : h_node_levels) {
+      ss << static_cast<int>(level) << ", ";
+    }
+    std::cerr << ss.str() << std::endl;
+  }
+#endif
+
   // Identify the key-value tokens.
+#if 0
 
   // Substring the input to extract out keys.
 
