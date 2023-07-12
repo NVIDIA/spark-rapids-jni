c77ab9d15751d10d9d4dc93890373eff21821518
nvdbaranec <56695930+nvdbaranec@users.noreply.github.com>
2023-07-12 10:02:04 -0500

    Add xxhash64 support (#1248)
    * Back port spark-specific murmur32 hash code from cudf.
    
    * Run pre-commit to format files. We were behind a bit.
    
    * Update pre-commit config to 16.0.1 to match cudf. Re-ran formatting.
    
    * Change jni bindings to use the spark-rapids-jni implementation of murmur hash instead of the cudf version.  Brought over
    cpp and java tests.
    
    * Documentation fix.
    
    * Fix cpp tests to actually call the spark_rapids_jni murmur hash.
    
    * First pass at xxhash64. cpp tests passing.
    
    * Improve cpp tests - null cases and more floating point edge cases.
    
    * Add Java tests.
    
    * Moved murmur32 hash implementaion from cudf to spark-rapids-jni
    
    Signed-off-by: db <dbaranec@nvidia.com>
    
    * PR review changes.
    
    * Fix copyright data in Hash.java
    
    * Enable 32 bit decimal hash test.
    
    * Implement xxhash64 on the gpu
    
    Signed-off-by: db <dbaranec@nvidia.com>
    
    * Add missing newlines.
    
    * PR review changes.
    
    * Remove default xxhash64 class constructor.  Remove unused parameter (row index) from remaining constructor.
    
    * Fix issues with merge.
    
    * Rectify thirdparty/cudf issues.
    
    * Revert inadvertent change to pom.xml
    
    * PR review feedback changes.
    
    ---------
    
    Signed-off-by: db <dbaranec@nvidia.com>

--- src/main/cpp/src/HashJni.cpp
+++ src/main/cpp/src/HashJni.cpp
@@ -36,4 +36,21 @@ JNIEXPORT jlong JNICALL Java_com_nvidia_spark_rapids_jni_Hash_murmurHash32(
   }
   CATCH_STD(env, 0);
 }
+
+JNIEXPORT jlong JNICALL Java_com_nvidia_spark_rapids_jni_Hash_xxhash64(JNIEnv* env,
+                                                                       jclass,
+                                                                       jlong seed,
+                                                                       jlongArray column_handles)
+{
+  JNI_NULL_CHECK(env, column_handles, "array of column handles is null", 0);
+
+  try {
+    cudf::jni::auto_set_device(env);
+    auto column_views =
+      cudf::jni::native_jpointerArray<cudf::column_view>{env, column_handles}.get_dereferenced();
+    return cudf::jni::release_as_jlong(
+      spark_rapids_jni::xxhash64(cudf::table_view{column_views}, seed));
+  }
+  CATCH_STD(env, 0);
+}
 }
--- src/main/java/com/nvidia/spark/rapids/jni/Hash.java
+++ src/main/java/com/nvidia/spark/rapids/jni/Hash.java
@@ -16,9 +16,15 @@
 
 package com.nvidia.spark.rapids.jni;
 
-import ai.rapids.cudf.*;
+import ai.rapids.cudf.ColumnVector;
+import ai.rapids.cudf.ColumnView;
+import ai.rapids.cudf.CudfException;
+import ai.rapids.cudf.NativeDepsLoader;
 
 public class Hash {
+  // there doesn't appear to be a useful constant in spark to reference. this could break.
+  static final long DEFAULT_XXHASH64_SEED = 42;
+
   static {
     NativeDepsLoader.loadNativeDeps();
   }
@@ -51,5 +57,35 @@ public class Hash {
     return murmurHash32(0, columns);
   }
 
+  /**
+   * Create a new vector containing the xxhash64 hash of each row in the table.
+   *
+   * @param seed integer seed for the xxhash64 hash function
+   * @param columns array of columns to hash, must have identical number of rows.
+   * @return the new ColumnVector of 64-bit values representing each row's hash value.
+   */
+  public static ColumnVector xxhash64(long seed, ColumnView columns[]) {
+    if (columns.length < 1) {
+      throw new IllegalArgumentException("xxhash64 hashing requires at least 1 column of input");
+    }
+    long[] columnViews = new long[columns.length];
+    long size = columns[0].getRowCount();
+
+    for(int i = 0; i < columns.length; i++) {
+      assert columns[i] != null : "Column vectors passed may not be null";
+      assert columns[i].getRowCount() == size : "Row count mismatch, all columns must be the same size";
+      assert !columns[i].getType().isDurationType() : "Unsupported column type Duration";
+      assert !columns[i].getType().isNestedType() : "Unsupported column type Nested";
+      columnViews[i] = columns[i].getNativeView(); 
+    }
+    return new ColumnVector(xxhash64(seed, columnViews));
+  }
+
+  public static ColumnVector xxhash64(ColumnView columns[]) {
+    return xxhash64(DEFAULT_XXHASH64_SEED, columns);
+  }
+
   private static native long murmurHash32(int seed, long[] viewHandles) throws CudfException;
+  
+  private static native long xxhash64(long seed, long[] viewHandles) throws CudfException;
 }
