5e790a802ff7643860a56ae2b409efa15cb6e43b
db <dbaranec@nvidia.com>
2023-07-19 14:42:11 -0500

    Add bloom_filter_put benchmark.  Fixed several benchmark build breakages.

--- src/main/cpp/src/murmur_hash.cuh
+++ src/main/cpp/src/murmur_hash.cuh
@@ -18,7 +18,10 @@
 
 #include "hash.cuh"
 
-#include <cudf/detail/utilities/hash_functions.cuh>
+#include <cudf/hashing.hpp>
+#include <cudf/hashing/detail/hash_functions.cuh>
+#include <cudf/replace.hpp>
+#include <cudf/strings/string_view.hpp>
 #include <cudf/types.hpp>
 
 namespace spark_rapids_jni {
@@ -76,10 +79,10 @@ struct MurmurHash3_32 {
       // casting byte-to-int, but C++ does not.
       uint32_t k1 = static_cast<uint32_t>(std::to_integer<int8_t>(data[i]));
       k1 *= c1;
-      k1 = cudf::detail::rotate_bits_left(k1, rot_c1);
+      k1 = cudf::hashing::detail::rotate_bits_left(k1, rot_c1);
       k1 *= c2;
       h ^= k1;
-      h = cudf::detail::rotate_bits_left(h, rot_c2);
+      h = cudf::hashing::detail::rotate_bits_left(h, rot_c2);
       h = h * 5 + c3;
     }
     return h;
@@ -96,10 +99,10 @@ struct MurmurHash3_32 {
     for (cudf::size_type i = 0; i < nblocks; i++) {
       uint32_t k1 = getblock32(data, i * BLOCK_SIZE);
       k1 *= c1;
-      k1 = cudf::detail::rotate_bits_left(k1, rot_c1);
+      k1 = cudf::hashing::detail::rotate_bits_left(k1, rot_c1);
       k1 *= c2;
       h ^= k1;
-      h = cudf::detail::rotate_bits_left(h, rot_c2);
+      h = cudf::hashing::detail::rotate_bits_left(h, rot_c2);
       h = h * 5 + c3;
     }
 
@@ -156,13 +159,13 @@ murmur_hash_value_type __device__ inline MurmurHash3_32<uint16_t>::operator()(
 template <>
 murmur_hash_value_type __device__ inline MurmurHash3_32<float>::operator()(float const& key) const
 {
-  return compute<float>(cudf::detail::normalize_nans(key));
+  return compute<float>(cudf::hashing::detail::normalize_nans(key));
 }
 
 template <>
 murmur_hash_value_type __device__ inline MurmurHash3_32<double>::operator()(double const& key) const
 {
-  return compute<double>(cudf::detail::normalize_nans(key));
+  return compute<double>(cudf::hashing::detail::normalize_nans(key));
 }
 
 template <>
