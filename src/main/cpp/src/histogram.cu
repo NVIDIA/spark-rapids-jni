0072c09a4a2ecb2653b2437ad138492f164274e4
Nghia Truong <7416935+ttnghia@users.noreply.github.com>
2023-10-17 14:46:00 -0700

    Patch parcentile evaluation to produce similar output as Spark CPU (#1506)
    * Prevent optimization
    
    * Add comment
    
    Signed-off-by: Nghia Truong <nghiat@nvidia.com>
    
    * Revert "Add comment"
    
    This reverts commit 025fad50bf317b8608e9f41ea637d3131181eadc.
    
    ---------
    
    Signed-off-by: Nghia Truong <nghiat@nvidia.com>

--- src/main/cpp/src/histogram.cu
+++ src/main/cpp/src/histogram.cu
@@ -97,7 +97,13 @@ struct fill_percentile_fn {
       return;
     }
 
-    output[idx] = (higher - position) * lower_element + (position - lower) * higher_element;
+    // Using `volatile` qualifier to prevent the compiler from combining `lower_part` and
+    // `upper_part` which may lead to output with different round-off error.
+    volatile double const lower_part =
+        (static_cast<double>(higher) - position) * static_cast<double>(lower_element);
+    volatile double const higher_part =
+        (position - static_cast<double>(lower)) * static_cast<double>(higher_element);
+    output[idx] = lower_part + higher_part;
   }
 
   fill_percentile_fn(cudf::size_type const *const offsets_, ElementIterator const sorted_input_,
