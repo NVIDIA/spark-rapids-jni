8c53cce969bc5db7bafd26ddc2431d208da4b0e3
Navin Kumar <navink@nvidia.com>
2023-11-09 12:11:57 -0800

    Refactor tests to use transitions as fixture

--- src/main/cpp/tests/timezones.cpp
+++ src/main/cpp/tests/timezones.cpp
@@ -29,25 +29,37 @@
 using seconds_col = 
     cudf::test::fixed_width_column_wrapper<cudf::timestamp_s, cudf::timestamp_s::rep>;
 
-struct TimeZoneTest : public cudf::test::BaseFixture {};
+class TimeZoneTest : public cudf::test::BaseFixture {
+protected:
+    void SetUp() override
+    {
+        transitions = make_transitions_table();
+    }
+    std::unique_ptr<cudf::table> transitions;
 
-TEST_F(TimeZoneTest, ConvertToUTC) {
-    auto instants_from_utc_col = cudf::test::fixed_width_column_wrapper<int64_t>({LONG_MIN, -650019600L, 515527200L, 684867600L});
-    auto instants_to_utc_col = cudf::test::fixed_width_column_wrapper<int64_t>({LONG_MIN, -649990800L, 515559600L, 684896400L});
-    auto utc_offsets_col = cudf::test::fixed_width_column_wrapper<int32_t>({29143, 28800, 32400, 28800});
-    auto struct_column = 
-        cudf::test::structs_column_wrapper{{instants_from_utc_col, instants_to_utc_col, utc_offsets_col}, {1, 1, 1, 1}};
-    auto offsets =
-        cudf::test::fixed_width_column_wrapper<cudf::size_type>{0, 4};
-    auto list_nullmask = std::vector<bool>(1, 1);
-    auto [null_mask, null_count] =
-        cudf::test::detail::make_null_mask(list_nullmask.begin(), list_nullmask.end());
-    auto list_column = cudf::make_lists_column(
-        1, offsets.release(), struct_column.release(), null_count, std::move(null_mask));
-    auto columns = std::vector<std::unique_ptr<cudf::column>>{};
-    columns.push_back(std::move(list_column));
-    auto transitions = cudf::table{std::move(columns)};
+private:
+    std::unique_ptr<cudf::table> make_transitions_table()
+    {
+        auto instants_from_utc_col = cudf::test::fixed_width_column_wrapper<int64_t>({LONG_MIN, LONG_MIN, -650019600L, 515527200L, 684867600L});
+        auto instants_to_utc_col = cudf::test::fixed_width_column_wrapper<int64_t>({LONG_MIN, LONG_MIN, -649990800L, 515559600L, 684896400L});
+        auto utc_offsets_col = cudf::test::fixed_width_column_wrapper<int32_t>({18000, 29143, 28800, 32400, 28800});
+        auto struct_column = 
+            cudf::test::structs_column_wrapper{{instants_from_utc_col, instants_to_utc_col, utc_offsets_col}, {1, 1, 1, 1, 1}};
+        auto offsets =
+            cudf::test::fixed_width_column_wrapper<cudf::size_type>{0, 1, 5};
+        auto list_nullmask = std::vector<bool>(1, 1);
+        auto [null_mask, null_count] =
+            cudf::test::detail::make_null_mask(list_nullmask.begin(), list_nullmask.end());
+        auto list_column = cudf::make_lists_column(
+            2, offsets.release(), struct_column.release(), null_count, std::move(null_mask));
+        auto columns = std::vector<std::unique_ptr<cudf::column>>{};
+        columns.push_back(std::move(list_column));
+        return std::make_unique<cudf::table>(std::move(columns));
+    }
+};
 
+TEST_F(TimeZoneTest, ConvertToUTC)
+{
     auto const ts_col = seconds_col{
         0L
     };
@@ -57,31 +69,16 @@ TEST_F(TimeZoneTest, ConvertToUTC) {
     };
     auto const actual = spark_rapids_jni::convert_timestamp_to_utc(
         ts_col,
-        transitions,
-        0,
+        *transitions,
+        1,
         cudf::get_default_stream(),
         rmm::mr::get_current_device_resource());
 
     CUDF_TEST_EXPECT_COLUMNS_EQUAL(expected, *actual);
 }
 
-TEST_F(TimeZoneTest, ConvertFromUTC) {
-    auto instants_from_utc_col = cudf::test::fixed_width_column_wrapper<int64_t>({LONG_MIN, -650019600L, 515527200L, 684867600L});
-    auto instants_to_utc_col = cudf::test::fixed_width_column_wrapper<int64_t>({LONG_MIN, -649990800L, 515559600L, 684896400L});
-    auto utc_offsets_col = cudf::test::fixed_width_column_wrapper<int32_t>({29143, 28800, 32400, 28800});
-    auto struct_column = 
-        cudf::test::structs_column_wrapper{{instants_from_utc_col, instants_to_utc_col, utc_offsets_col}, {1, 1, 1, 1}};
-    auto offsets =
-        cudf::test::fixed_width_column_wrapper<cudf::size_type>{0, 4};
-    auto list_nullmask = std::vector<bool>(1, 1);
-    auto [null_mask, null_count] =
-        cudf::test::detail::make_null_mask(list_nullmask.begin(), list_nullmask.end());
-    auto list_column = cudf::make_lists_column(
-        1, offsets.release(), struct_column.release(), null_count, std::move(null_mask));
-    auto columns = std::vector<std::unique_ptr<cudf::column>>{};
-    columns.push_back(std::move(list_column));
-    auto transitions = cudf::table{std::move(columns)};
-
+TEST_F(TimeZoneTest, ConvertFromUTC)
+{
     auto const ts_col = seconds_col{
         0L
     };
@@ -91,8 +88,8 @@ TEST_F(TimeZoneTest, ConvertFromUTC) {
     };
     auto const actual = spark_rapids_jni::convert_utc_timestamp_to_timezone(
         ts_col,
-        transitions,
-        0,
+        *transitions,
+        1,
         cudf::get_default_stream(),
         rmm::mr::get_current_device_resource());
 
