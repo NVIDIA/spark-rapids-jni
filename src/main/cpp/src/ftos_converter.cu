360a77bb87a83ca6bcd96f2637fb477118fdffe8
Haoyang Li <ntlihy@gmail.com>
2023-11-21 23:55:53 +0800

    Update src/main/cpp/src/ftos_converter.cu
    Co-authored-by: Jason Lowe <jlowe@nvidia.com>

--- src/main/cpp/src/ftos_converter.cu
+++ src/main/cpp/src/ftos_converter.cu
@@ -1,21 +1,5 @@
-// Copyright 2018 Ulf Adams
-//
-// The contents of this file may be used under the terms of the Apache License,
-// Version 2.0.
-//
-//    (See accompanying file LICENSE-Apache or copy at
-//     http://www.apache.org/licenses/LICENSE-2.0)
-//
-// Alternatively, the contents of this file may be used under the terms of
-// the Boost Software License, Version 1.0.
-//    (See accompanying file LICENSE-Boost or copy at
-//     https://www.boost.org/LICENSE_1_0.txt)
-//
-// Unless required by applicable law or agreed to in writing, this software
-// is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied.
-
 /*
+ * Copyright 2018 Ulf Adams
  * Copyright (c) 2023, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
