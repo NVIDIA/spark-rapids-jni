6b092495311e345f8fdea23915cf9c192d34037c
Haoyang Li <haoyangl@nvidia.com>
2024-01-16 17:34:33 +0800

    clean up
    Signed-off-by: Haoyang Li <haoyangl@nvidia.com>

--- src/main/cpp/src/timezones.cu
+++ src/main/cpp/src/timezones.cu
@@ -31,8 +31,6 @@
 #include <rmm/cuda_stream_view.hpp>
 #include <rmm/exec_policy.hpp>
 
-#include <optional>
-
 using column                   = cudf::column;
 using column_device_view       = cudf::column_device_view;
 using column_view              = cudf::column_view;
@@ -132,8 +130,8 @@ struct time_add_functor {
 
   int64_t const duration_scalar;
 
-  __device__ inline timestamp_type gao(timestamp_type const& timestamp,
-                                       int64_t const& duration) const
+  __device__ inline timestamp_type plus_with_tz(timestamp_type const& timestamp,
+                                                int64_t const& duration) const
   {
     if (duration == 0L) { return timestamp; }
 
@@ -143,7 +141,7 @@ struct time_add_functor {
 
     auto const epoch_seconds_utc = static_cast<int64_t>(
       cuda::std::chrono::duration_cast<cudf::duration_s>(timestamp.time_since_epoch()).count());
-    // input_timestamp
+    // input epoch seconds
 
     auto const tz_transitions = cudf::list_device_view{transitions, tz_index};
     auto const list_size      = tz_transitions.size();
@@ -156,14 +154,14 @@ struct time_add_functor {
       tz_instants.data<int64_t>() + tz_transitions.element_offset(0),
       static_cast<size_t>(list_size));
 
-    // step 1: Get offset when converting local timestamp to utc
+    // step 1: Binary search on utc to find the correct offset to convert utc to local
     auto const utc_it = thrust::upper_bound(
       thrust::seq, transition_times_utc.begin(), transition_times_utc.end(), epoch_seconds_utc);
-    auto utc_idx = static_cast<size_type>(thrust::distance(transition_times_utc.begin(), utc_it));
+    auto const utc_idx =
+      static_cast<size_type>(thrust::distance(transition_times_utc.begin(), utc_it));
     auto const utc_list_offset = tz_transitions.element_offset(utc_idx - 1);
     auto const to_local_offset =
       static_cast<int64_t>(utc_offsets.element<int32_t>(utc_list_offset));
-    // offset
 
     auto const to_local_offset_duration = cuda::std::chrono::duration_cast<duration_type>(
       cudf::duration_s{static_cast<int64_t>(to_local_offset)});
@@ -171,18 +169,27 @@ struct time_add_functor {
     auto const duration_typed = cuda::std::chrono::duration_cast<duration_type>(
       cudf::duration_us{static_cast<int64_t>(duration)});
 
+    // step 2: add the duration to the local timestamp
     auto const local_timestamp_res = timestamp + to_local_offset_duration + duration_typed;
 
     auto const result_epoch_seconds = static_cast<int64_t>(
       cuda::std::chrono::duration_cast<cudf::duration_s>(local_timestamp_res.time_since_epoch())
         .count());
 
+    // step 3: Binary search on local to find the correct offset to convert local to utc
     auto const local_it = thrust::upper_bound(
       thrust::seq, transition_times_tz.begin(), transition_times_tz.end(), result_epoch_seconds);
     auto local_idx =
       static_cast<size_type>(thrust::distance(transition_times_tz.begin(), local_it));
+    // In GpuTimeZoneDB, we build the transition list with
+    // (utcInstant, utcInstant + OffsetBefore, OffsetAfter) if it is a overlap.
+    // But in the step 3, we actually need to binary search on the utcInstant + OffsetBefore
+    // to find the correct offset to convert local to utc. To reuse the code, we need to
+    // add a special post processing here, to make sure we get the correct id that
+    // utcInstant + OffsetBefore is larger than the result_epoch_seconds.
     auto const temp_list_offset = tz_transitions.element_offset(local_idx);
     auto const temp_offset = static_cast<int64_t>(utc_offsets.element<int32_t>(temp_list_offset));
+    // We don't want to check this if the idx is the first or last
     if (local_idx != 0 && transition_times_utc[local_idx] != INT64_MAX &&
         transition_times_utc[local_idx] + temp_offset <= result_epoch_seconds) {
       local_idx += 1;
@@ -192,14 +199,15 @@ struct time_add_functor {
     auto const upper_bound_epoch = transition_times_tz[local_idx - 1];
     auto const upper_bound_utc   = transition_times_utc[local_idx - 1];
 
+    // step 4: if the result is in the overlap, try to select the original offset if possible
     auto const early_offset = static_cast<int64_t>(upper_bound_epoch - upper_bound_utc);
-
-    bool const is_gap     = (upper_bound_utc + to_utc_offset == upper_bound_epoch);
-    bool const is_overlap = !is_gap && upper_bound_utc != INT64_MIN;
-    if (is_overlap) {  // overlap
+    bool const is_gap       = (upper_bound_utc + to_utc_offset == upper_bound_epoch);
+    if (!is_gap && upper_bound_utc != INT64_MIN && upper_bound_utc != INT64_MAX) {  // overlap
+      // The overlap range is [utcInstant + offsetBefore, utcInstant + offsetAfter]
       auto const overlap_before = static_cast<int64_t>(upper_bound_utc + to_utc_offset);
-      auto const overlap_after  = static_cast<int64_t>(upper_bound_epoch);
-      if (result_epoch_seconds >= overlap_before && result_epoch_seconds <= overlap_after) {
+      if (result_epoch_seconds >= overlap_before && result_epoch_seconds <= upper_bound_epoch) {
+        // By default, to_utc_offset is the offsetAfter, so if the to_local_offset is valid and
+        // need to replace the to_utc_offset, it only happens when it is early_offset
         if (to_local_offset == early_offset) { to_utc_offset = early_offset; }
       }
     }
@@ -207,18 +215,19 @@ struct time_add_functor {
     auto const to_utc_offset_duration = cuda::std::chrono::duration_cast<duration_type>(
       cudf::duration_s{static_cast<int64_t>(to_utc_offset)});
 
+    // step 5: subtract the offset to convert local to utc
     return local_timestamp_res - to_utc_offset_duration;
   }
 
   __device__ timestamp_type operator()(timestamp_type const& timestamp) const
   {
-    return gao(timestamp, duration_scalar);
+    return plus_with_tz(timestamp, duration_scalar);
   }
 
   __device__ timestamp_type operator()(timestamp_type const& timestamp,
                                        int64_t const& interval) const
   {
-    return gao(timestamp, interval);
+    return plus_with_tz(timestamp, interval);
   }
 };
 
