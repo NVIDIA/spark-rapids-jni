1d27b332b1252031ff1efd412e4f98a556c37daf
Mike Wilson <hyperbolic2346@users.noreply.github.com>
2024-01-24 18:41:35 -0500

    Add support for `parse_uri` to limit query with a column (#1719)
    * Adding support for parse uri for query with a column for keys
    
    Signed-off-by: Mike Wilson <knobby@burntsheep.com>
    Co-authored-by: nvdbaranec <56695930+nvdbaranec@users.noreply.github.com>

--- src/test/java/com/nvidia/spark/rapids/jni/ParseURITest.java
+++ src/test/java/com/nvidia/spark/rapids/jni/ParseURITest.java
@@ -124,6 +124,41 @@ public class ParseURITest {
     }
   }
 
+  void testQuery(String[] testData, String[] params) {
+    String[] expectedQueryStrings = new String[testData.length];
+    for (int i=0; i<testData.length; i++) {
+      String query = null;
+      try {
+        URI uri = new URI(testData[i]);
+        query = uri.getRawQuery();
+      } catch (URISyntaxException ex) {
+        // leave the query null if URI is invalid
+      } catch (NullPointerException ex) {
+        // leave the query null if URI is null
+      }
+
+      String subquery = null;
+
+      if (query != null) {
+        String[] pairs = query.split("&");
+        for (String pair : pairs) {
+          int idx = pair.indexOf("=");
+          if (idx > 0 && pair.substring(0, idx).equals(params[i])) {
+            subquery = pair.substring(idx + 1);
+            break;
+          }
+        }
+      }
+      expectedQueryStrings[i] = subquery;
+    }
+    try (ColumnVector v0 = ColumnVector.fromStrings(testData);
+      ColumnVector p0 = ColumnVector.fromStrings(params);
+      ColumnVector expectedQuery = ColumnVector.fromStrings(expectedQueryStrings);
+      ColumnVector queryResult = ParseURI.parseURIQueryWithColumn(v0, p0)) {
+      AssertUtils.assertColumnsAreEqual(expectedQuery, queryResult);
+    }
+  }
+
   @Test
   void parseURISparkTest() {
     String[] testData = {
@@ -180,11 +215,68 @@ public class ParseURITest {
       "userinfo@www.nvidia.com/path?query=1#Ref",
       "",
       null};
+  
+
+      String[] queries = {
+        "a",
+        "h",
+        // commented out until https://github.com/NVIDIA/spark-rapids/issues/10036 is fixed
+        //"object",
+        "object",
+        "a",
+        "h",
+        "a",
+        "f",
+        "g",
+        "a",
+        "a",
+        "f",
+        "g",
+        "a",
+        "a",
+        "b",
+        "a",
+        "",
+        "a",
+        "a",
+        "a",
+        "a",
+        "b",
+        "a",
+        "q",
+        "b",
+        "a",
+        "query",
+        "a",
+        "primekey_in",
+        "a",
+        "q",
+        "ExpertId",
+        "query",
+        "solutionId",
+        "f",
+        "param",
+        "",
+        "q",
+        "a",
+        "f",
+        "mnid=5080",
+        "f",
+        "a",
+        "param4",
+        "cloth",
+        "a",
+        "invalid",
+        "invalid",
+        "query",
+        "a",
+        "f"};
 
     testProtocol(testData);
     testHost(testData);
     testQuery(testData);
     testQuery(testData, "query");
+    testQuery(testData, queries);
   }
 
   @Test
