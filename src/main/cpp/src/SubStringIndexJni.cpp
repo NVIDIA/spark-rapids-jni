68ff75f65b2f2d6ec97569aeb41fb38655c1a0a5
fejiang <fejiang@nvidia.com>
2024-07-07 14:32:39 +0800

    modified ColumnView.java adding methods
    Signed-off-by: fejiang <fejiang@nvidia.com>

--- src/main/cpp/src/SubStringIndexJni.cpp
+++ src/main/cpp/src/SubStringIndexJni.cpp
@@ -20,18 +20,18 @@
 extern "C" {
 
 JNIEXPORT jlong JNICALL Java_com_nvidia_spark_rapids_jni_SubstringIndex_substringIndexWithScalar(
-  JNIEnv* env, jclass, jlong strings_handle, jlong delimiter_handle, jint count)
+  JNIEnv* env, jclass, jlong strings_handle, jstring delimiter_object, jint count)
 {
   JNI_NULL_CHECK(env, strings_handle, "strings column handle is null", 0);
   JNI_NULL_CHECK(env, delimiter_handle, "delimiter scalar handle is null", 0);
   try {
     cudf::jni::auto_set_device(env);
-    cudf::strings_column_view strings_view{
-      *reinterpret_cast<cudf::column_view const*>(strings_handle)};
-    // auto strings_view = reinterpret_cast<cudf::strings_column_view*>(strings_handle);
-    auto delimiter_scalar = reinterpret_cast<cudf::string_scalar*>(delimiter_handle);
+    auto const input = reinterpret_cast<cudf::column_view const*>(input_handle);
+    auto const strings_column = cudf::strings_column_view{*input};
+    auto const delimiter_jstr = cudf::jni::native_jstring(env, delimiter_obj);
+    auto const delimiter = std::string(delimiter_jstr.get(), delimiter_jstr.size_bytes());
     return cudf::jni::release_as_jlong(
-      spark_rapids_jni::substring_index(strings_view, *delimiter_scalar, count));
+      spark_rapids_jni::substring_index(strings_column, cudf::string_scalar{delimiter}, count));
   }
   CATCH_STD(env, 0);
 }
