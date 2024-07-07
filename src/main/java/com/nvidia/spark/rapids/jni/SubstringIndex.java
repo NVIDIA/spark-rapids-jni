cf7be6980a308232c3ea3b99b97ac210117ea19a
fejiang <fejiang@nvidia.com>
2024-07-07 15:00:03 +0800

    substringIndex.java
    Signed-off-by: fejiang <fejiang@nvidia.com>

---
+++ src/main/java/com/nvidia/spark/rapids/jni/SubstringIndex.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2022-2024, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.nvidia.spark.rapids.jni;
+
+import ai.rapids.cudf.*;
+
+public class SubstringIndex {
+    static{
+        NativeDepsLoader.loadNativeDeps();
+    }
+
+    public static ColumnVector substringIndex(ColumnView cv, String delimiter, int count){
+        return new ColumnVector(substring_index(cv.getNativeView(), delimiter, count));
+    }
+
+
+    public static ColumnVector substringIndex(ColumnView cv, ColumnView delimiterView, int count){
+        assert delimiterView.getType().equals(DType.STRING) : "column type must be a String";
+        return new ColumnVector(substring_index(cv.getNativeView(), delimiterView.getNativeView(), count));
+    }
+
+
+    private static native long substring_index(long columnView, String delimiter, int count) throws CudfException;
+
+    private static native long substring_index(long columnView, long delimiterView, int count) throws CudfException;
+
+
+}
