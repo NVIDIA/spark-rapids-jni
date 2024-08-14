515bb6b559b3a7801ac8d96fc52513e47a8ccafb
Nghia Truong <nghiat@nvidia.com>
2024-08-14 14:39:23 -0700

    Fix style
    Signed-off-by: Nghia Truong <nghiat@nvidia.com>

--- src/main/cpp/src/from_json_to_raw_map.cu
+++ src/main/cpp/src/from_json_to_raw_map.cu
@@ -70,7 +70,7 @@ rmm::device_uvector<char> unify_json_strings(cudf::strings_column_view const& in
   auto const d_strings  = cudf::column_device_view::create(input.parent(), stream);
   auto const chars_size = input.chars_size(stream);
   auto const output_size =
-    2l +  // two extra bracket characters '[' and ']'
+    2l +                                            // two extra bracket characters '[' and ']'
     static_cast<int64_t>(chars_size) +
     static_cast<int64_t>(input.size() - 1) +        // append `,` character between input rows
     static_cast<int64_t>(input.null_count()) * 2l;  // replace null with "{}"
