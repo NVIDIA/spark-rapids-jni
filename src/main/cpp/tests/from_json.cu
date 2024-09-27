de53ef06c9c288d2733d695ccb85512405be0eb5
Nghia Truong <nghiat@nvidia.com>
2024-09-27 13:26:49 -0700

    Add test

--- src/main/cpp/tests/from_json.cu
+++ src/main/cpp/tests/from_json.cu
@@ -159,6 +159,30 @@ TEST_F(FromJsonTest, T33)
   }
 }
 
+TEST_F(FromJsonTest, T34)
+{
+  // The last row is invalid (has an extra quote).
+  auto const json_string = cudf::test::strings_column_wrapper{"{'data': [0]}"};
+
+  spark_rapids_jni::json_schema_element a{cudf::data_type{cudf::type_id::LIST}, {}};
+  a.child_types.emplace_back(
+    "", spark_rapids_jni::json_schema_element{cudf::data_type{cudf::type_id::STRING}, {}});
+
+  std::vector<std::pair<std::string, spark_rapids_jni::json_schema_element>> schema;
+  schema.emplace_back("data", std::move(a));
+
+  auto const output = spark_rapids_jni::from_json_to_structs(
+    cudf::strings_column_view{json_string}, schema, false, false, false);
+
+  printf("\n\ninput: \n");
+  cudf::test::print(json_string);
+
+  printf("\n\noutput: \n");
+  for (auto const& col : output) {
+    cudf::test::print(col->view());
+  }
+}
+
 TEST_F(FromJsonTest, T4)
 {
   // The last row is invalid (has an extra quote).
