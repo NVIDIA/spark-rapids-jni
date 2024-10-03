f10e73a26dcd5daba827369e89fd207db72241db
Nghia Truong <nghiat@nvidia.com>
2024-10-03 15:26:49 -0700

    Reorganize code
    Signed-off-by: Nghia Truong <nghiat@nvidia.com>

--- src/main/cpp/src/json_utils.cu
+++ src/main/cpp/src/json_utils.cu
@@ -71,71 +71,12 @@ std::tuple<std::unique_ptr<cudf::column>, std::unique_ptr<rmm::device_buffer>, c
   rmm::cuda_stream_view stream,
   rmm::device_async_resource_ref mr)
 {
-  auto const d_input_ptr = cudf::column_device_view::create(input.parent(), stream);
-  auto const default_mr  = rmm::mr::get_current_device_resource();
-
-  // Check if the input rows are either null, equal to `null` string literal, or empty.
-  // This will be used for masking out the input when doing string concatenation.
-  rmm::device_uvector<bool> is_valid_input(input.size(), stream, default_mr);
-
-  // Check if the input rows are either null or empty.
-  // This will be returned to the caller.
-  rmm::device_uvector<bool> is_null_or_empty(input.size(), stream, mr);
-
-  thrust::transform(
-    rmm::exec_policy_nosync(stream),
-    thrust::make_counting_iterator(0),
-    thrust::make_counting_iterator(input.size()),
-    thrust::make_zip_iterator(thrust::make_tuple(is_valid_input.begin(), is_null_or_empty.begin())),
-    [input = *d_input_ptr] __device__(cudf::size_type idx) -> thrust::tuple<bool, bool> {
-      if (input.is_null(idx)) { return {false, true}; }
-
-      auto const d_str = input.element<cudf::string_view>(idx);
-      auto const size  = d_str.size_bytes();
-      int i            = 0;
-      char ch;
-
-      // Skip the very first whitespace characters.
-      for (; i < size; ++i) {
-        ch = d_str[i];
-        if (not_whitespace(ch)) { break; }
-      }
-
-      bool is_null_literal{false};
-      if (i + 3 < size &&
-          (d_str[i] == 'n' && d_str[i + 1] == 'u' && d_str[i + 2] == 'l' && d_str[i + 3] == 'l')) {
-        is_null_literal = true;
-        i += 4;
-      }
-
-      // Skip the very last whitespace characters.
-      for (; i < size; ++i) {
-        ch = d_str[i];
-        if (not_whitespace(ch)) {
-          is_null_literal = false;
-          break;
-        }
-      }
-
-      // The current row contains only `null` string literal and not any other non-empty characters.
-      // Such rows need to be masked out as null when doing concatenation.
-      if (is_null_literal) { return {false, false}; }
-
-      auto const not_eol = i < size;
-
-      // If the current row is not null or empty, it should start with `{`. Otherwise, we need to
-      // replace it by a null. This is necessary for libcudf's JSON reader to work.
-      // Note that if we want to support ARRAY schema, we need to check for `[` instead.
-      auto constexpr start_character = '{';
-      if (not_eol && ch != start_character) { return {false, false}; }
-
-      return {not_eol, !not_eol};
-    });
-
   auto constexpr num_levels  = 256;
   auto constexpr lower_level = std::numeric_limits<char>::min();
   auto constexpr upper_level = std::numeric_limits<char>::max();
-  auto const num_chars       = input.chars_size(stream);
+
+  auto const num_chars  = input.chars_size(stream);
+  auto const default_mr = rmm::mr::get_current_device_resource();
 
   rmm::device_uvector<uint32_t> histogram(num_levels, stream, default_mr);
   thrust::uninitialized_fill(
@@ -185,6 +126,66 @@ std::tuple<std::unique_ptr<cudf::column>, std::unique_ptr<rmm::device_buffer>, c
   }
   auto const delimiter = static_cast<char>(thrust::distance(zero_level_it, first_zero_count_pos));
 
+  // Check if the input rows are either null, equal to `null` string literal, or empty.
+  // This will be used for masking out the input when doing string concatenation.
+  rmm::device_uvector<bool> is_valid_input(input.size(), stream, default_mr);
+
+  // Check if the input rows are either null or empty.
+  // This will be returned to the caller.
+  rmm::device_uvector<bool> is_null_or_empty(input.size(), stream, mr);
+
+  auto const d_input_ptr = cudf::column_device_view::create(input.parent(), stream);
+  thrust::transform(
+    rmm::exec_policy_nosync(stream),
+    thrust::make_counting_iterator(0),
+    thrust::make_counting_iterator(input.size()),
+    thrust::make_zip_iterator(thrust::make_tuple(is_valid_input.begin(), is_null_or_empty.begin())),
+    [input = *d_input_ptr] __device__(cudf::size_type idx) -> thrust::tuple<bool, bool> {
+      if (input.is_null(idx)) { return {false, true}; }
+
+      auto const d_str = input.element<cudf::string_view>(idx);
+      auto const size  = d_str.size_bytes();
+      int i            = 0;
+      char ch;
+
+      // Skip the very first whitespace characters.
+      for (; i < size; ++i) {
+        ch = d_str[i];
+        if (not_whitespace(ch)) { break; }
+      }
+
+      bool is_null_literal{false};
+      if (i + 3 < size &&
+          (d_str[i] == 'n' && d_str[i + 1] == 'u' && d_str[i + 2] == 'l' && d_str[i + 3] == 'l')) {
+        is_null_literal = true;
+        i += 4;
+      }
+
+      // Skip the very last whitespace characters.
+      if (is_null_literal) {
+        for (; i < size; ++i) {
+          ch = d_str[i];
+          if (not_whitespace(ch)) {
+            is_null_literal = false;
+            break;
+          }
+        }
+      }
+
+      // The current row contains only `null` string literal and not any other non-empty characters.
+      // Such rows need to be masked out as null when doing concatenation.
+      if (is_null_literal) { return {false, false}; }
+
+      auto const not_eol = i < size;
+
+      // If the current row is not null or empty, it should start with `{`. Otherwise, we need to
+      // replace it by a null. This is necessary for libcudf's JSON reader to work.
+      // Note that if we want to support ARRAY schema, we need to check for `[` instead.
+      auto constexpr start_character = '{';
+      if (not_eol && ch != start_character) { return {false, false}; }
+
+      return {not_eol, !not_eol};
+    });
   auto [null_mask, null_count] = cudf::detail::valid_if(
     is_valid_input.begin(), is_valid_input.end(), thrust::identity{}, stream, default_mr);
   // If the null count doesn't change, that mean we do not have any rows containing `null` string
