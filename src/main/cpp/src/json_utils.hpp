ee4a6c4fb8e8507b91f107b596c3eac80d069db6
Nghia Truong <7416935+ttnghia@users.noreply.github.com>
2024-11-05 09:39:39 -0800

    Improve `concat_json` (#2557)
    * Add `nullify_invalid_rows` parameter to `concat_json`
    
    Signed-off-by: Nghia Truong <nghiat@nvidia.com>
    
    * Cleanup and add more docs
    
    Signed-off-by: Nghia Truong <nghiat@nvidia.com>
    
    * Rename variable
    
    Signed-off-by: Nghia Truong <nghiat@nvidia.com>
    
    ---------
    
    Signed-off-by: Nghia Truong <nghiat@nvidia.com>

--- src/main/cpp/src/json_utils.hpp
+++ src/main/cpp/src/json_utils.hpp
@@ -32,14 +32,35 @@ std::unique_ptr<cudf::column> from_json_to_raw_map(
   rmm::cuda_stream_view stream      = cudf::get_default_stream(),
   rmm::device_async_resource_ref mr = rmm::mr::get_current_device_resource());
 
-std::tuple<std::unique_ptr<cudf::column>, std::unique_ptr<rmm::device_buffer>, char> concat_json(
-  cudf::strings_column_view const& input,
-  rmm::cuda_stream_view stream      = cudf::get_default_stream(),
-  rmm::device_async_resource_ref mr = rmm::mr::get_current_device_resource());
-
 std::unique_ptr<cudf::column> make_structs(
   std::vector<cudf::column_view> const& input,
   cudf::column_view const& is_null,
+  rmm::cuda_stream_view stream      = cudf::get_default_stream(),
+  rmm::device_async_resource_ref mr = rmm::mr::get_current_device_resource());
+
+/**
+ * @brief Concatenate the JSON objects given by a strings column into one single character buffer,
+ * in which each JSON objects is delimited by a special character that does not exist in the input.
+ *
+ * Beyond returning the concatenated buffer with delimiter, the function also returns a BOOL8
+ * column indicating which rows should be nullified after parsing the concatenated buffer. Each
+ * row of this column is a `true` value if the corresponding input row is either empty, containing
+ * only whitespaces, or invalid JSON object depending on the `nullify_invalid_rows` parameter.
+ *
+ * Note that an invalid JSON object in this context is a string that does not start with the `{`
+ * character after whitespaces.
+ *
+ * @param input The strings column containing input JSON objects
+ * @param nullify_invalid_rows Whether to nullify rows containing invalid JSON objects
+ * @param stream The CUDA stream used for device memory operations and kernel launches
+ * @param mr Device memory resource used to allocate device memory of the table in the returned
+ * @return A tuple containing the concatenated JSON objects as a single buffer, the delimiter
+ *         character, and a BOOL8 column indicating which rows should be nullified after parsing
+ *         the concatenated buffer
+ */
+std::tuple<std::unique_ptr<rmm::device_buffer>, char, std::unique_ptr<cudf::column>> concat_json(
+  cudf::strings_column_view const& input,
+  bool nullify_invalid_rows         = false,
   rmm::cuda_stream_view stream      = cudf::get_default_stream(),
   rmm::device_async_resource_ref mr = rmm::mr::get_current_device_resource());
 
