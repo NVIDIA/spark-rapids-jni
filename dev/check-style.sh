27e9ce40408235793cd1971355bbb5dfd184f3e4
liurenjie1024 <liurenjie2008@gmail.com>
2024-11-20 17:13:27 +0800

    Add checkstyle in maven build

---
+++ dev/check-style.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -ex
+
+# Assuming you are in the root of your git repository
+MODIFIED_FILES=$(git diff --name-only HEAD~1)
+
+SRC_DIR="src/main/java/"
+TEST_SRC_DIR="src/test/java/"
+# Filter out the Java files that have been modified
+JAVA_FILES=()
+for FILE in $MODIFIED_FILES; do
+  if [[ $FILE == *.java ]]; then
+    if [[ $FILE == $SRC_DIR* ]]; then
+      JAVA_FILES+=("${FILE#"$SRC_DIR"}") # Remove the src/main/java/ prefix
+    elif [[ $FILE == $TEST_SRC_DIR* ]]; then
+      JAVA_FILES+=("${FILE#"$TEST_SRC_DIR"}") # Remove the src/test/java/ prefix
+    fi
+  fi
+done
+
+# If there are Java files to check, run Checkstyle on them
+if [ ${#JAVA_FILES[@]} -ne 0 ]; then
+  mvn checkstyle:check -Dcheckstyle.includes="$(echo "${JAVA_FILES[@]}" | tr ' ' ',')"
+else
+  echo "No Java files modified, skipping Checkstyle."
+fi
