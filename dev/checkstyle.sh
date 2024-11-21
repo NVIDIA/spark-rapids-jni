574a33e55bae80b6746c7fc043a62c033e39c72a
liurenjie1024 <liurenjie2008@gmail.com>
2024-11-21 11:24:48 +0800

    Fix header

--- dev/checkstyle.sh
+++ dev/checkstyle.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#
 # Copyright (c) 2024, NVIDIA CORPORATION. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,7 +14,6 @@
 # limitations under the License.
 #
 
-#!/bin/bash
 
 set -ex
 
