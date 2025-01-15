a7da1824646fa4cfb5bef8cb49a40eae89b0ef05
Yan Feng <fengyan_@mail.ustc.edu.cn>
2025-01-15 12:03:39 +0800

    Remove template variable LongType
    Signed-off-by: Yan Feng <fengyan_@mail.ustc.edu.cn>

--- src/main/cpp/src/cast_long_to_binary_string.cu
+++ src/main/cpp/src/cast_long_to_binary_string.cu
@@ -22,7 +22,6 @@
 #include <cudf/detail/nvtx/ranges.hpp>
 #include <cudf/strings/detail/strings_children.cuh>
 #include <cudf/utilities/default_stream.hpp>
-#include <cudf/utilities/type_dispatcher.hpp>
 
 #include <rmm/cuda_stream_view.hpp>
 #include <rmm/exec_policy.hpp>
@@ -31,22 +30,15 @@ namespace spark_rapids_jni {
 
 namespace detail {
 namespace {
-
-template <typename LongType>
 struct long_to_binary_string_fn {
   cudf::column_device_view d_longs;
   cudf::size_type* d_sizes;
   char* d_chars;
   cudf::detail::input_offsetalator d_offsets;
 
-  __device__ cudf::size_type compute_output_size(LongType value)
-  {
-    return max(64 - __clzll(value), 1);  // If the value is 0, the output size should be 1
-  }
-
   __device__ void long_to_binary_string(cudf::size_type idx)
   {
-    auto const value = d_longs.element<LongType>(idx);
+    auto const value = d_longs.element<int64_t>(idx);
     char* d_buffer   = d_chars + d_offsets[idx];
     for (auto i = d_sizes[idx] - 1; i >= 0; --i) {
       *d_buffer++ = value & (1LL << i) ? '1' : '0';
@@ -62,35 +54,9 @@ struct long_to_binary_string_fn {
     if (d_chars != nullptr) {
       long_to_binary_string(idx);
     } else {
-      d_sizes[idx] = compute_output_size(d_longs.element<LongType>(idx));
+      // If the value is 0, the size should be 1
+      d_sizes[idx] = max(1, 64 - __clzll(d_longs.element<int64_t>(idx)));
     }
-  }
-};
-
-struct dispatch_long_to_binary_string_fn {
-  template <typename LongType, CUDF_ENABLE_IF(std::is_same_v<LongType, std::int64_t>)>
-  std::unique_ptr<cudf::column> operator()(cudf::column_view const& input,
-                                           rmm::cuda_stream_view stream,
-                                           rmm::device_async_resource_ref mr) const
-  {
-    auto const d_column = cudf::column_device_view::create(input, stream);
-
-    auto [offsets, chars] = cudf::strings::detail::make_strings_children(
-      long_to_binary_string_fn<LongType>{*d_column}, input.size(), stream, mr);
-
-    return cudf::make_strings_column(input.size(),
-                                     std::move(offsets),
-                                     chars.release(),
-                                     input.null_count(),
-                                     cudf::detail::copy_bitmask(input, stream, mr));
-  }
-
-  template <typename LongType, CUDF_ENABLE_IF(not std::is_same_v<LongType, std::int64_t>)>
-  std::unique_ptr<cudf::column> operator()(cudf::column_view const&,
-                                           rmm::cuda_stream_view,
-                                           rmm::device_async_resource_ref) const
-  {
-    CUDF_FAIL("Values for long_to_binary_string function must be a long type.");
   }
 };
 
@@ -101,7 +67,19 @@ std::unique_ptr<cudf::column> long_to_binary_string(cudf::column_view const& inp
                                                     rmm::device_async_resource_ref mr)
 {
   if (input.is_empty()) return cudf::make_empty_column(cudf::type_id::STRING);
-  return type_dispatcher(input.type(), dispatch_long_to_binary_string_fn{}, input, stream, mr);
+
+  CUDF_EXPECTS(input.type().id() == cudf::type_id::INT64, "Input column must be long type");
+
+  auto const d_column = cudf::column_device_view::create(input, stream);
+
+  auto [offsets, chars] = cudf::strings::detail::make_strings_children(
+    long_to_binary_string_fn{*d_column}, input.size(), stream, mr);
+
+  return cudf::make_strings_column(input.size(),
+                                   std::move(offsets),
+                                   chars.release(),
+                                   input.null_count(),
+                                   cudf::detail::copy_bitmask(input, stream, mr));
 }
 
 }  // namespace detail
