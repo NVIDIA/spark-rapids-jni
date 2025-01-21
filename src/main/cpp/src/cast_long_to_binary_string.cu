e7cbf469a7ee9ba76be768f55f83e7faaad00ed1
Yan Feng <fengyan_@mail.ustc.edu.cn>
2025-01-21 14:48:49 +0800

    Add comments for long_to_binary_string_kernel
    Signed-off-by: Yan Feng <fengyan_@mail.ustc.edu.cn>

--- src/main/cpp/src/cast_long_to_binary_string.cu
+++ src/main/cpp/src/cast_long_to_binary_string.cu
@@ -43,6 +43,29 @@ CUDF_KERNEL void compute_output_size_kernel(cudf::column_device_view d_longs,
   d_sizes[tid] = max(1, 64 - __clzll(d_longs.element<int64_t>(tid)));
 }
 
+/**
+ * @brief CUDA kernel to convert long to binary string representation.
+ *
+ * Each long is processed by `num_threads_per_row` threads.
+ * Each thread processes `64 / num_threads_per_row` bits.
+ * `bit_index` begins from LSB to MSB, and `char_index` equals to `str_len - 1 - bit_index`.
+ *
+ * For example, if `num_threads_per_row` is 32, then each thread processes 2 bits of the long.
+ * Suppose the input is 25L, which is 0b 0000...0001_1001, and the output size should be 5.
+ *
+ * lane  0 handles bit_indices [ 0,  1], write '1' to char_index 4 and '0' to char_index 3
+ * lane  1 handles bit_indices [ 2,  3], write '0' to char_index 2 and '1' to char_index 1
+ * lane  2 handles bit_indices [ 4,  5], write '1' to char_index 0, and '0' won't be written
+ * since bit_index 5 >= str_len
+ * ...
+ * lane 31 handles bit_indices [62, 63], the smallest bit_index exceeds str_len, no write operation
+ *
+ * The output binary string will be "11001".
+ *
+ * @param d_longs Device view of the input column containing longs.
+ * @param d_chars Pointer to the output character array where binary strings will be stored.
+ * @param d_offsets Input offset calculator to manage the offsets in the output character array.
+ */
 template <int num_threads_per_row>
 CUDF_KERNEL void long_to_binary_string_kernel(cudf::column_device_view d_longs,
                                               char* d_chars,
@@ -58,14 +81,14 @@ CUDF_KERNEL void long_to_binary_string_kernel(cudf::column_device_view d_longs,
   constexpr int num_bits_per_thread = 64 / num_threads_per_row;
   auto const value                  = d_longs.element<int64_t>(row_idx);
   auto const lane_idx               = tid % num_threads_per_row;
-  auto const first_byte_index       = lane_idx * num_bits_per_thread;
+  auto const first_bit_index        = lane_idx * num_bits_per_thread;
 
   char* d_buffer = d_chars + d_offsets[row_idx];
-  for (auto byte_index = first_byte_index;
-       byte_index < first_byte_index + num_bits_per_thread && byte_index < str_len;
-       ++byte_index) {
-    auto const num_shifts = str_len - 1 - byte_index;
-    d_buffer[byte_index]  = '0' + ((value & (1UL << num_shifts)) >> num_shifts);
+  for (auto bit_index = first_bit_index;
+       bit_index < first_bit_index + num_bits_per_thread && bit_index < str_len;
+       ++bit_index) {
+    auto const char_index = str_len - 1 - bit_index;
+    d_buffer[char_index]  = '0' + ((value & (1ULL << bit_index)) >> bit_index);
   }
 }
 
@@ -75,9 +98,9 @@ std::unique_ptr<cudf::column> long_to_binary_string(cudf::column_view const& inp
                                                     rmm::cuda_stream_view stream,
                                                     rmm::device_async_resource_ref mr)
 {
-  if (input.is_empty()) return cudf::make_empty_column(cudf::type_id::STRING);
-
   CUDF_EXPECTS(input.type().id() == cudf::type_id::INT64, "Input column must be long type");
+
+  if (input.is_empty()) return cudf::make_empty_column(cudf::type_id::STRING);
 
   auto const strings_count = input.size();
   auto const d_column      = cudf::column_device_view::create(input, stream);
