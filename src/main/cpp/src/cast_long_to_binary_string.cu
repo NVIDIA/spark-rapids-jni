835f193b9696be92080af7284e318b693dea78fc
Yan Feng <fengyan_@mail.ustc.edu.cn>
2025-01-21 11:14:04 +0800

    Optimize long_to_binary_string function
    Signed-off-by: Yan Feng <fengyan_@mail.ustc.edu.cn>

--- src/main/cpp/src/cast_long_to_binary_string.cu
+++ src/main/cpp/src/cast_long_to_binary_string.cu
@@ -30,35 +30,44 @@ namespace spark_rapids_jni {
 
 namespace detail {
 namespace {
-struct long_to_binary_string_fn {
-  cudf::column_device_view d_longs;
-  cudf::size_type* d_sizes;
-  char* d_chars;
-  cudf::detail::input_offsetalator d_offsets;
-
-  __device__ void long_to_binary_string(cudf::size_type idx)
-  {
-    auto const value = d_longs.element<int64_t>(idx);
-    char* d_buffer   = d_chars + d_offsets[idx];
-    for (auto i = d_sizes[idx] - 1; i >= 0; --i) {
-      *d_buffer++ = '0' + ((value & (1UL << i)) >> i);
-    }
+CUDF_KERNEL void compute_output_size_kernel(cudf::column_device_view d_longs,
+                                            cudf::size_type* d_sizes)
+{
+  auto const tid = cudf::detail::grid_1d::global_thread_id();
+  if (tid >= d_longs.size()) { return; }
+  if (d_longs.is_null(tid)) {
+    d_sizes[tid] = 0;
+    return;
   }
+  // If the value is 0, the size should be 1
+  d_sizes[tid] = max(1, 64 - __clzll(d_longs.element<int64_t>(tid)));
+}
 
-  __device__ void operator()(cudf::size_type idx)
-  {
-    if (d_longs.is_null(idx)) {
-      if (d_chars == nullptr) { d_sizes[idx] = 0; }
-      return;
-    }
-    if (d_chars != nullptr) {
-      long_to_binary_string(idx);
-    } else {
-      // If the value is 0, the size should be 1
-      d_sizes[idx] = max(1, 64 - __clzll(d_longs.element<int64_t>(idx)));
-    }
+CUDF_KERNEL void long_to_binary_string_kernel(cudf::column_device_view d_longs,
+                                              char* d_chars,
+                                              cudf::detail::input_offsetalator d_offsets,
+                                              cudf::size_type num_threads_per_row)
+{
+  auto const tid     = cudf::detail::grid_1d::global_thread_id();
+  auto const row_idx = tid / num_threads_per_row;
+  if (row_idx >= d_longs.size()) { return; }
+
+  auto const str_len = d_offsets[row_idx + 1] - d_offsets[row_idx];
+  if (str_len == 0) { return; }
+
+  auto const value               = d_longs.element<int64_t>(row_idx);
+  auto const lane_idx            = tid % num_threads_per_row;
+  auto const num_bits_per_thread = 64 / num_threads_per_row;
+  auto const first_byte_index    = lane_idx * num_bits_per_thread;
+
+  char* d_buffer = d_chars + d_offsets[row_idx];
+  for (auto i = 0; i < num_bits_per_thread; ++i) {
+    auto const byte_index = first_byte_index + i;
+    if (byte_index >= str_len) { return; }
+    auto const num_shifts = str_len - 1 - byte_index;
+    d_buffer[byte_index]  = '0' + ((value & (1UL << num_shifts)) >> num_shifts);
   }
-};
+}
 
 }  // namespace
 
@@ -70,10 +79,36 @@ std::unique_ptr<cudf::column> long_to_binary_string(cudf::column_view const& inp
 
   CUDF_EXPECTS(input.type().id() == cudf::type_id::INT64, "Input column must be long type");
 
-  auto const d_column = cudf::column_device_view::create(input, stream);
+  auto const strings_count = input.size();
+  auto const d_column      = cudf::column_device_view::create(input, stream);
 
-  auto [offsets, chars] = cudf::strings::detail::make_strings_children(
-    long_to_binary_string_fn{*d_column}, input.size(), stream, mr);
+  // The following code is adapted from `cudf::strings::detail::make_strings_children()`
+  // Compute the output sizes
+  auto output_sizes         = rmm::device_uvector<cudf::size_type>(strings_count, stream);
+  cudf::size_type* d_sizes  = output_sizes.data();
+  auto constexpr block_size = 256;
+  auto grid                 = cudf::detail::grid_1d{strings_count, block_size};
+  compute_output_size_kernel<<<grid.num_blocks, block_size, 0, stream.value()>>>(*d_column,
+                                                                                 d_sizes);
+
+  // Convert the sizes to offsets
+  auto [offsets, bytes] = cudf::strings::detail::make_offsets_child_column(
+    output_sizes.begin(), output_sizes.end(), stream, mr);
+  cudf::detail::input_offsetalator d_offsets =
+    cudf::detail::offsetalator_factory::make_input_iterator(offsets->view());
+
+  // Now build the chars column
+  rmm::device_uvector<char> chars(bytes, stream, mr);
+  cudf::experimental::prefetch::detail::prefetch("gather", chars, stream);
+  char* d_chars = chars.data();
+
+  // Fill in the chars data
+  auto constexpr num_threads_per_row = 32;
+  auto new_grid = cudf::detail::grid_1d{strings_count * num_threads_per_row, block_size};
+  if (bytes > 0) {
+    long_to_binary_string_kernel<<<new_grid.num_blocks, block_size, 0, stream.value()>>>(
+      *d_column, d_chars, d_offsets, num_threads_per_row);
+  }
 
   return cudf::make_strings_column(input.size(),
                                    std::move(offsets),
