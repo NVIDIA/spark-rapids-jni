81010b344facfd10132ade742751118dd857f7af
Robert (Bobby) Evans <bobby@apache.org>
2025-02-11 10:27:08 -0600

    Updated test and some debugging a little
    Signed-off-by: Robert (Bobby) Evans <bobby@apache.org>

--- src/test/java/com/nvidia/spark/rapids/jni/kudo/KudoGpuSerializerTest.java
+++ src/test/java/com/nvidia/spark/rapids/jni/kudo/KudoGpuSerializerTest.java
@@ -72,8 +72,9 @@ public class KudoGpuSerializerTest {
   @Test
   public void testSinglePartWriteCPURead() throws Exception {
     try (Table table = new Table.TestBuilder()
-        .column((byte)0xFF, (byte)0xF0, (byte)0x0F, (byte)0xAA, (byte)0x55)
-        .column((byte)0xFF, (byte)0xF0, (byte)0x0F, (byte)0xAA, (byte)0x55)
+        //.column(null, (byte)0xF0, (byte)0x0F, (byte)0xAA, null)
+        //.column((short)0xFFFF, (short)0xF0F0, null, (short)0xAAAA, (short)0x5555)
+        .column("0xFF", null, "0x0F", "0xAA", "0x55")
         .build()) {
       DeviceMemoryBuffer[] buffers = KudoGpuSerializer.splitAndSerializeToDevice(table);
       assertEquals(2, buffers.length);
@@ -81,8 +82,9 @@ public class KudoGpuSerializerTest {
            DeviceMemoryBuffer offsets = buffers[1]) {
         // Ignoring the offsets for now because it should just be the start to the end of the buffer (one split)
         Schema s = Schema.builder()
-            .column(DType.INT8, "a")
-            .column(DType.INT32, "b")
+          //  .column(DType.INT8, "a")
+           // .column(DType.INT16, "b")
+            .column(DType.STRING, "c")
             .build();
         KudoSerializer serializer = new KudoSerializer(s);
         ByteArrayOutputStream tmpOut = new ByteArrayOutputStream();
@@ -93,20 +95,68 @@ public class KudoGpuSerializerTest {
           tmp.copyFromDeviceBuffer(data);
           tmp.getBytes(hData, 0, 0, hData.length);
         }
-        System.err.println("COMP GPU(" + hData.length + ") vs CPU(" + hDataCPU.length + ")");
+        System.err.println("     COMP GPU(" + hData.length + ") VS CPU(" + hDataCPU.length + ")");
         int len = Math.max(hData.length, hDataCPU.length);
+        int hasValidLen = -1;
         for (int i = 0; i < len; i++) {
+          Byte gpuByte = null;
+          Byte cpuByte = null;
           String gpu = "N/A     ";
           if (i < hData.length) {
+            gpuByte = hData[i];
             gpu = String.format("0x%02X %03d", hData[i] & 0xFF, hData[i]);
           }
 
           String cpu = "N/A     ";
           if (i < hDataCPU.length) {
+            cpuByte = hDataCPU[i];
             cpu = String.format("0x%02X %03d", hDataCPU[i] & 0xFF, hDataCPU[i]);
           }
 
-          System.err.println(i + ": " + gpu + " VS " + cpu);
+          String diff = "   ";
+          if (cpuByte != gpuByte) {
+            diff = "***";
+          }
+          String extra = "";
+          if (i == 0) {
+            extra = " <-- MAGIC START";
+          } else if (i == 3) {
+            extra = " <-- MAGIC END";
+          } else if (i == 4) {
+            extra = " <-- OFFSET START";
+          } else if (i == 7) {
+            extra = " <-- OFFSET END";
+          } else if (i == 8) {
+            extra = " <-- NUM_ROWS START";
+          } else if (i == 11) {
+            extra = " <-- NUM_ROWS END";
+          } else if (i == 12) {
+            extra = " <-- VALIDITY_BUF_LEN START";
+          } else if (i == 15) {
+            extra = " <-- VALIDITY_BUF_LEN END";
+          } else if (i == 16) {
+            extra = " <-- OFFSET_BUF_LEN START";
+          } else if (i == 19) {
+            extra = " <-- OFFSET_BUF_LEN END";
+          } else if (i == 20) {
+            extra = " <-- TOTAL_DATA_LEN START";
+          } else if (i == 23) {
+            extra = " <-- TOTAL_DATA_LEN END";
+          } else if (i == 24) {
+            extra = " <-- NUM_COL START";
+          } else if (i == 27) {
+            extra = " <-- NUM_COL END";
+            int gpuNumCol = (hData[24] << 24) + (hData[25] << 16) + (hData[26] << 8) + hData[27];
+            hasValidLen = (gpuNumCol + 7) / 8;
+          } else if (hasValidLen == 1 && i == 28) {
+            extra = " <-- HAS_VALID_BUF";
+          } else if (hasValidLen > 1 && i == 28) {
+            extra = " <-- HAS_VALID_BUF START";
+          } else if (hasValidLen > 1 && i == (27 + hasValidLen)) {
+            extra = " <-- HAS_VALID_BUF END";
+          }
+          String i_str = String.format("%04d: ", i);
+          System.err.println(i_str + diff + " " + gpu + " VS " + cpu  + extra);
         }
         // TODO verify that there is nothing more to read
         ByteArrayInputStream bin = new ByteArrayInputStream(hData);
