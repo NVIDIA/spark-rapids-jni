62f595a95259bd0d7523e757bd6bb2ff45d0f7b7
liurenjie1024 <liurenjie2008@gmail.com>
2025-02-12 11:08:38 +0800

    Simplify header

--- src/main/java/com/nvidia/spark/rapids/jni/kudo/KudoTableMerger.java
+++ src/main/java/com/nvidia/spark/rapids/jni/kudo/KudoTableMerger.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024, NVIDIA CORPORATION.
+ * Copyright (c) 2024-2025, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -219,7 +219,6 @@ class KudoTableMerger implements SchemaVisitor2 {
 
   /**
    * Copy a sliced validity buffer to the destination buffer, starting at the given bit offset.
-   *
    * Visible for testing.
    *
    * @return Number of nulls in the validity buffer.
