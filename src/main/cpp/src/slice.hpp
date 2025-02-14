a4fd39b4b784ec9b516a7e23ddf17081dd290f61
ustcfy <96854327+ustcfy@users.noreply.github.com>
2025-02-14 16:15:19 +0800

    Update src/main/cpp/src/slice.hpp
    Co-authored-by: Chong Gao <gaochong.gc@qq.com>

--- src/main/cpp/src/slice.hpp
+++ src/main/cpp/src/slice.hpp
@@ -19,7 +19,9 @@
 #include <cudf/column/column.hpp>
 #include <cudf/lists/lists_column_view.hpp>
 
-#include <rmm/mr/device/per_device_resource.hpp>
+#include <cudf/utilities/default_stream.hpp>
+#include <cudf/utilities/memory_resource.hpp>
+
 
 namespace spark_rapids_jni {
 
