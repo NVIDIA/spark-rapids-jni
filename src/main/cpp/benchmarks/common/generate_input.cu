065114fde967f179daf794809fae4644b5147207
Nghia Truong <nghiat@nvidia.com>
2025-06-12 10:46:47 -0700

    Fix compile issue
    Signed-off-by: Nghia Truong <nghiat@nvidia.com>

--- src/main/cpp/benchmarks/common/generate_input.cu
+++ src/main/cpp/benchmarks/common/generate_input.cu
@@ -133,7 +133,7 @@ size_t non_fixed_width_size<cudf::struct_view>(data_profile const& profile)
   return std::accumulate(dist_params.leaf_types.cbegin(),
                          dist_params.leaf_types.cend(),
                          0ul,
-                         [&](auto& sum, auto type_id) {
+                         [&](auto sum, auto type_id) {
                            return sum + avg_element_size(profile, cudf::data_type{type_id});
                          });
 }
