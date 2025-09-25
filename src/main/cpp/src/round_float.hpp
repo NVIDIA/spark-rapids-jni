3ec8b5e63471a6c1caab9867b5a4c48a426c8326
Paul Mattione <pmattione@nvidia.com>
2025-09-25 13:35:37 -0400

    Copy cudf floating point rounding code into spark-rapids-jni, implement new API, change tests to use it instead of old code

---
+++ src/main/cpp/src/round_float.hpp
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2025, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+#pragma once
+
+#include <cudf/column/column.hpp>
+#include <cudf/column/column_view.hpp>
+#include <cudf/round.hpp>
+#include <cudf/types.hpp>
+#include <cudf/utilities/default_stream.hpp>
+#include <cudf/utilities/memory_resource.hpp>
+
+#include <rmm/cuda_stream_view.hpp>
+#include <rmm/resource_ref.hpp>
+
+namespace spark_rapids_jni {
+
+/**
+ * @brief Rounds all the values in a column to the specified number of decimal places.
+ *
+ * `cudf::round` currently supports HALF_UP and HALF_EVEN rounding for integer, floating point and
+ * `decimal32` and `decimal64` numbers. For `decimal32` and `decimal64` numbers, negated
+ * `numeric::scale` is equivalent to `decimal_places`.
+ *
+ * Example:
+ * ```
+ * cudf::column_view a; // contains { 1.729, 17.29, 172.9, 1729 };
+ *
+ * auto result1 = round(a);     // { 2,   17,   173,   1729 }
+ * auto result2 = round(a, 1);  // { 1.7, 17.3, 172.9, 1729 }
+ * auto result3 = round(a, -1); // { 0,   20,   170,   1730 }
+ *
+ * cudf::column_view b; // contains { 1.5, 2.5, 1.35, 1.45, 15, 25 };
+ *
+ * auto result4 = round(b,  0, cudf::rounding_method::HALF_EVEN); // { 2,   2,   1,   1,   15, 25};
+ * auto result5 = round(b,  1, cudf::rounding_method::HALF_EVEN); // { 1.5, 2.5, 1.4, 1.4, 15, 25};
+ * auto result6 = round(b, -1, cudf::rounding_method::HALF_EVEN); // { 0,   0,   0,   0,   20, 20};
+ * ```
+ *
+ * @param input          Column of values to be rounded
+ * @param decimal_places Number of decimal places to round to (default 0). If negative, this
+ * specifies the number of positions to the left of the decimal point.
+ * @param method         Rounding method
+ * @param stream         CUDA stream used for device memory operations and kernel launches
+ * @param mr             Device memory resource used to allocate the returned column's device memory
+ *
+ * @return Column with each of the values rounded
+ */
+ std::unique_ptr<cudf::column> round(
+    cudf::column_view const& input,
+    int32_t decimal_places            = 0,
+    cudf::rounding_method method      = cudf::rounding_method::HALF_UP,
+    rmm::cuda_stream_view stream      = cudf::get_default_stream(),
+    rmm::device_async_resource_ref mr = cudf::get_current_device_resource_ref());
+  
+ } // namespace spark_rapids_jni
