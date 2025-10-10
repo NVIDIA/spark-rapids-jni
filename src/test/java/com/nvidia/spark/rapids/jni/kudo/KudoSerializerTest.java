15d796be8fbe1f337bb335f1baaccbc2735a2d5f
liurenjie1024 <liurenjie2008@gmail.com>
2025-10-10 13:14:01 +0800

    Debug

--- src/test/java/com/nvidia/spark/rapids/jni/kudo/KudoSerializerTest.java
+++ src/test/java/com/nvidia/spark/rapids/jni/kudo/KudoSerializerTest.java
@@ -19,6 +19,7 @@ package com.nvidia.spark.rapids.jni.kudo;
 import ai.rapids.cudf.*;
 import com.nvidia.spark.rapids.jni.Arms;
 import java.util.Arrays;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
@@ -51,6 +52,14 @@ public class KudoSerializerTest extends CudfTestBase {
 
   public KudoSerializerTest() {
     super(RmmAllocationMode.POOL, RMM_POOL_SIZE_LARGE);
+  }
+
+  @AfterAll
+  public static void afterAll() throws Exception {
+    if (Rmm.isInitialized()) {
+      Rmm.shutdown();
+    }
+    Thread.sleep(60000); // give some time for cleaner threads to finish
   }
 
   @Test
