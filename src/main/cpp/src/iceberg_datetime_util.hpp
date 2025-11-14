658db0b8d73c62e4f6c7dce51b6e1cfc5f907b96
Chong Gao <res_life@163.com>
2025-11-14 14:00:16 +0800

    optimize includes
    Signed-off-by: Chong Gao <res_life@163.com>

--- src/main/cpp/src/iceberg_datetime_util.hpp
+++ src/main/cpp/src/iceberg_datetime_util.hpp
@@ -18,9 +18,8 @@
 
 #include <cudf/column/column.hpp>
 #include <cudf/column/column_view.hpp>
-
-#include <rmm/cuda_stream_view.hpp>
-#include <rmm/resource_ref.hpp>
+#include <cudf/utilities/default_stream.hpp>
+#include <cudf/utilities/memory_resource.hpp>
 
 #include <memory>
 
