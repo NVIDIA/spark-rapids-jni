0e82ca8614c89dcb00218f1598e492a9195dbe67
Haoyang Li <haoyangl@nvidia.com>
2026-03-17 17:04:28 +0800

    fix compile
    Signed-off-by: Haoyang Li <haoyangl@nvidia.com>

--- src/test/java/com/nvidia/spark/rapids/jni/ProtobufTest.java
+++ src/test/java/com/nvidia/spark/rapids/jni/ProtobufTest.java
@@ -25,6 +25,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
